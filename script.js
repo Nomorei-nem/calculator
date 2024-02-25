035bd2c3f9c29f6d1e7faa21b6ad2e5cb30199e7
NomoreiMe <eldomatt2017@gmail.com>
2024-02-25 17:31:16 +0530

    Style calculator

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const keys = document.querySelectorAll('.key');
+const keyStroke = new Audio(
+	'./assets/mixkit-single-key-press-in-a-laptop-2541.wav'
+);
+
+keys.forEach((e) => {
+	e.addEventListener('click', () => keyStroke.play());
+});
+
+console.log(keys);
